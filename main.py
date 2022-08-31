9d1d4a34ae9a6cebd695321ccb3b29054c6fdb3c
cesar214567 <camgcamg11@gmail.com>
2022-08-31 12:58:47 -0500

    changing ports

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ def test_AI():
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(port=8080, threaded=True, host=('0.0.0.1'))
